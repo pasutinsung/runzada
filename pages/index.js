43f42643414c3f4d0eb9793c250b517ef2ab6aae
pasutinsung <pasut.run@gmail.com>
2023-01-24 19:25:19 +0700

    create ProductItem

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,8 @@ import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "@next/font/google";
 import Layout from "@/components/Layout";
+import data from "@/utils/data";
+import ProductItem from "@/components/ProductItem";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -10,7 +12,13 @@ export default function Home() {
     <>
       <header></header>
       <main>
-        <Layout>Hello</Layout>
+        <Layout>
+          <div className="grid grid-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-4">
+            {data.products.map((products) => (
+              <ProductItem product={products} key={products.slug}></ProductItem>
+            ))}
+          </div>
+        </Layout>
       </main>
     </>
   );
