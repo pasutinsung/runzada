97675f5b6f537b6fbb99db46269371a08c0395b3
pasutinsung <pasut.run@gmail.com>
2023-01-25 18:17:03 +0700

    save cart

--- pages/cart.js
+++ pages/cart.js
@@ -1,11 +1,11 @@
 import Layout from "@/components/Layout";
 import Link from "next/link";
 import React, { useContext } from "react";
-import { XCircleIcon } from "@heroicons/react/outline";
 import { Store } from "@/utils/Stores";
 import Image from "next/image";
 import { router, useRouter } from "next/router";
-export default function CartScreen() {
+import dynamic from "next/dynamic";
+function CartScreen() {
   const router = useRouter();
   const { state, dispatch } = useContext(Store);
   const {
@@ -13,6 +13,10 @@ export default function CartScreen() {
   } = state;
   const removeItemHandler = (item) => {
     dispatch({ type: "CART_REMOVE_ITEM", payload: item });
+  };
+  const updateCartHandler = (item, qty) => {
+    const quantity = Number(qty);
+    dispatch({ type: "CART_ADD_ITEM", payload: { ...item, quantity } });
   };
   return (
     <Layout title="Shopping cart">
@@ -49,7 +53,20 @@ export default function CartScreen() {
                         </div>
                       </Link>
                     </td>
-                    <td className="p-5 text-right">{item.quantity}</td>
+                    <td className="p-5 text-right">
+                      <select
+                        value={item.cartItems}
+                        onChange={(e) =>
+                          updateCartHandler(item, e.target.value)
+                        }
+                      >
+                        {[...Array(item.countInStock).keys()].map((x) => (
+                          <option key={x + 1} value={x + 1}>
+                            {x + 1}
+                          </option>
+                        ))}
+                      </select>
+                    </td>
                     <td className="p-5 text-right">{item.price}$</td>
                     <td className="p-5 text-center">
                       <button onClick={() => removeItemHandler(item)}>
@@ -98,3 +115,4 @@ export default function CartScreen() {
     </Layout>
   );
 }
+export default dynamic(() => Promise.resolve(CartScreen), { ssr: false });
